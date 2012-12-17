820d603a614740fbbaac1323c79846383d0458c6
Timothy Wong <masterchung7@gmail.com>
2012-12-16 23:43:06 -0600

    add pa stuff

--- cm.mk
+++ cm.mk
@@ -1,15 +1,18 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit PA stuff
+$(call inherit-product, vendor/pa/config/pa_common.mk)
+
 # Boot animation
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+TARGET_BOOTANIMATION_NAME := vertical-480x800
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/shadow/shadow.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := shadow
-PRODUCT_NAME := cm_shadow
+PRODUCT_NAME := pa_shadow
 PRODUCT_BRAND := verizon
 PRODUCT_MODEL := DROIDX
 PRODUCT_MANUFACTURER := Motorola
