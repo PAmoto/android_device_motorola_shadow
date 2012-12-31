b7b9c5675ee1e035a8dd7cc6fdac96c391710dc0
Timothy Wong <masterchung7@gmail.com>
2012-12-31 11:28:36 -0600

    accidentally deleted this repo, and updates

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit PA stuff
 $(call inherit-product, vendor/pa/config/pa_common.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOTANIMATION_NAME := vertical-480x800
